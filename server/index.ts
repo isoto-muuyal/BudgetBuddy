7e05acdca389100c7a450eb32cf8be53b7ac0c98
isoto soto <israelss@gmail.com>
2026-01-24 21:43:56 -0500

    vite not loading

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite-helpers";
 
 const app = express();
 
@@ -63,6 +63,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    const { setupVite } = await import("./vite");
     await setupVite(app, server);
   } else {
     serveStatic(app);
